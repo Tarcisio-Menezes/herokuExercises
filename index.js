69811bf13479f2cdc4813b14dcf0a8daaee157c0
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-15 14:06:48 -0300

    corrige dotenv, adiciona mensagem

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-require('dotenv');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+const SECRET = process.env.SECRET || 'Deu ruim';
 
 app.get('/', (_req, res) => {
-  res.status(200).send('Estoy vivo!')
+  res.status(200).send(`Estoy vivo! ${SECRET}`)
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Fala comigo na porta ${PORT}`)
